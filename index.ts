869cb3442b285479dd18c24be87469554df589fb
Fabien Ancelin <fancelin@esri.ca>
2023-06-05 21:42:36 -0400

    Not working yet.

---
+++ index.ts
@@ -0,0 +1,11 @@
+import {
+  RowSelectionSingle,
+  RowSelectionMultiple,
+} from './src/rowSelection/rowSelection';
+import * as RowSelectionActions from './src/rowSelection/actions';
+
+export {
+  RowSelectionSingle,
+  RowSelectionMultiple,
+  RowSelectionActions,
+};
